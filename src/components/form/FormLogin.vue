b58481c30b838791db5e1a57ed313cac005dd994
Jeanella <jeanellach@gmail.com>
2023-11-18 18:26:25 -0500

    primera pantalla

---
+++ src/components/form/FormLogin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Inicia sesión</h2>
+    <form @submit="submitForm">
+      <div>
+        <label for="email">Correo electrónico:</label>
+        <input type="email" id="email" v-model="email" required />
+      </div>
+      <div>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <button type="submit">Iniciar sesión</button>
+    </form>
+    <p>¿Eres nuevo aquí? <a href="#">Crea una cuenta</a></p>
+  </div>
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import FormLogin from '../components/form/FormLogin.vue'
+</script>
+
+<template>
+  <main class="container-main">
+    <div class="container-main__layout">
+      <div class="container-main__front-page">
+        <img
+          class="container-main__img-employees"
+          alt="empleados analizando data"
+          src="../assets/login.png"
+        />
+        <div class="container-main__line-green"></div>
+        <div class="container-main__front-text">
+          <img src="../assets/logo_white.svg" alt="logo de culqui" />
+          <h1>Dale más power⚡a tus empleados hoy 💪</h1>
+        </div>
+      </div>
+      <div class="container-main__form">
+        <FormLogin></FormLogin>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.container-main {
+  min-height: 100vh;
+  &__layout {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  &__img-employees {
+    height: 100%;
+    width: 100%;
+  }
+
+  &__line-green {
+    height: 20px;
+    background-color: green;
+  }
+}
+</style>
